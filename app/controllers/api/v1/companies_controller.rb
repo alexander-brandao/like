8f8967679cd16110808d9ad06ecb1d0a399a7521
Alex <alex@alex-linux.meuintelbras.local>
2022-01-04 20:46:49 -0300

    controllers part 2

--- app/controllers/api/v1/companies_controller.rb
+++ app/controllers/api/v1/companies_controller.rb
@@ -1,6 +1,56 @@
 module Api
     module V1
         class CompaniesController < ApplicationController
+            def index
+                companies = Company.all
+                
+                render json: CompanySerializer.new(companies).serialized_json
+            end
+
+            def show
+                company = Company.find_by(slug: params[:slug])
+
+                render json: CompanySerializer.new(company).serialized_json
+
+            end
+            
+            def create
+                company = Company.new(company_params)
+
+                if company.save
+                    render json: CompanySerializer.new(company).serialized_json
+                else
+                    render json: {error: company.errors.messages }, status: 422
+                end
+
+            end
+
+            def update
+                company = Company.find_by(slug: params)
+
+                if company.update(company_params)
+                    render json: CompanySerializer.new(company).serialized_json
+                else
+                    render json: {error: company.errors.messages }, status: 422
+                end
+                
+            end
+
+            def destroy
+                company = Company.find_by(slug: params)
+
+                if company.destroy
+                    head :no_content
+                else
+                    render json: {error: company.errors.messages }, status: 422
+                end
+                
+            end
+
+            private
+            def company_params
+                params.require(:company).permit(:name, :image_url)
+            end
         end
         
     end
